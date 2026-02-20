774ad27946f77ee4c6b4c18fca8590f4a37287c3
SararRomero <hsromerosara@gmail.com>
2026-02-20 15:41:15 -0500

    Se hacen los ejercicios de streamlit y se añade un archivo pruebas para reciclar codigo

---
+++ pages/pruebas.py
@@ -0,0 +1,42 @@
+#st.subheader("Ejercicio 2: Calculadora de Producto")
+
+#num1 = st.number_input("Número 1", value=0)
+#num2 = st.number_input("Número 2", value=0)
+
+#resultado = num1 * num2
+#st.write(f"Resultado: {resultado}")
+
+#if num1 > 100 or num2 > 100:
+ #   st.warning("Números grandes")
+
+#st.divider()
+
+# st.subheader("Ejercicio 1: Calculadora Básica")
+
+# num1 = st.number_input("Número 1", value=0.0)
+# num2 = st.number_input("Número 2", value=0.0)
+
+# #  Warning si hay números grandes
+# if num1 > 100 or num2 > 100:
+#     st.warning("Números grandes")
+
+# operacion = st.selectbox(
+#     "Selecciona la operación",
+#     ["Sumar", "Restar", "Multiplicar", "Dividir"]
+# )
+
+# if operacion == "Sumar":
+#     resultado = num1 + num2
+# elif operacion == "Restar":
+#     resultado = num1 - num2
+# elif operacion == "Multiplicar":
+#     resultado = num1 * num2
+# elif operacion == "Dividir":
+#     if num2 != 0:
+#         resultado = num1 / num2
+#     else:
+#         resultado = "No se puede dividir entre 0"
+
+# st.write(f"Resultado: {resultado}")
+
+# st.divider()
